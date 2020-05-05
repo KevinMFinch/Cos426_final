c74ea95af54bbe5355e9a046abac5aa5a5113fc4
Kevin <finchkevinm74@gmail.com>
2020-05-05 12:47:30 -0400

    Add walls and floor

--- src/components/scenes/SeedScene.js
+++ src/components/scenes/SeedScene.js
@@ -1,5 +1,5 @@
 import * as Dat from 'dat.gui';
-import { Scene, Color } from 'three';
+import { Scene, Color, PlaneGeometry, MeshBasicMaterial, Mesh, DoubleSide, Plane } from 'three';
 import { Flower, Land } from 'objects';
 import { BasicLights } from 'lights';
 
@@ -22,7 +22,33 @@ class SeedScene extends Scene {
         const land = new Land();
         const flower = new Flower(this);
         const lights = new BasicLights();
-        this.add(land, flower, lights);
+        flower.position.set(2, 0, 2);
+
+        const floorGeometry = new PlaneGeometry(200, 100, 1);
+        floorGeometry.rotateX(-Math.PI / 2);
+
+        const shortWallGeometry = new PlaneGeometry(100, 5, 1);
+        const longWallGeometry = new PlaneGeometry(200, 5, 1);
+
+        const floorMat = new MeshBasicMaterial({color: 0x33DDFF, side: DoubleSide});
+        const wallMat = new MeshBasicMaterial({color: 0xffddcc, side: DoubleSide});
+
+        const floorPlane = new Mesh(floorGeometry, floorMat);
+
+        const wallPlaneTop = new Mesh(longWallGeometry, wallMat);
+        const wallPlaneBot = new Mesh(longWallGeometry, wallMat);
+        const wallPlaneRight = new Mesh(shortWallGeometry, wallMat);
+        const wallPlaneLeft = new Mesh(shortWallGeometry, wallMat);
+
+        wallPlaneTop.position.set(0, 0, 50);
+        wallPlaneBot.position.set(0, 0, -50);
+        wallPlaneRight.position.set(100, 0, 0);
+        wallPlaneRight.rotateY(Math.PI / 2);
+        wallPlaneLeft.position.set(-100, 0, 0);
+        wallPlaneLeft.rotateY(Math.PI / 2);
+
+        const wallPlanes = [wallPlaneTop, wallPlaneBot, wallPlaneRight, wallPlaneLeft];
+        this.add(land, flower, floorPlane, ...wallPlanes, lights);
 
         // Populate GUI
         this.state.gui.add(this.state, 'rotationSpeed', -5, 5);
@@ -35,7 +61,6 @@ class SeedScene extends Scene {
     update(timeStamp) {
         const { rotationSpeed, updateList } = this.state;
         this.rotation.y = (rotationSpeed * timeStamp) / 10000;
-
         // Call update for each object in the updateList
         for (const obj of updateList) {
             obj.update(timeStamp);
