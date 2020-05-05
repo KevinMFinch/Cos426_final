f0b1dd361022f704af27728719969239d7d54dcb
Alice <hanjixue@gmail.com>
2020-05-05 12:55:31 -0400

    add motorcycles

--- src/components/scenes/SeedScene.js
+++ src/components/scenes/SeedScene.js
@@ -1,6 +1,6 @@
 import * as Dat from 'dat.gui';
 import { Scene, Color } from 'three';
-import { Flower, Land } from 'objects';
+import { Flower, Land, Motorcycle } from 'objects';
 import { BasicLights } from 'lights';
 
 class SeedScene extends Scene {
@@ -22,7 +22,9 @@ class SeedScene extends Scene {
         const land = new Land();
         const flower = new Flower(this);
         const lights = new BasicLights();
-        this.add(land, flower, lights);
+        const redMotor = new Motorcycle(this);
+        debugger;
+        this.add(land, flower, lights, redMotor);
 
         // Populate GUI
         this.state.gui.add(this.state, 'rotationSpeed', -5, 5);
