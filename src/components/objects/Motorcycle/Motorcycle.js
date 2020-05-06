056fb77304141000343b8dd3c880259a069d1489
Kevin <finchkevinm74@gmail.com>
2020-05-06 13:52:02 -0400

    Add constants and set board size accordingly

--- src/components/objects/Motorcycle/Motorcycle.js
+++ src/components/objects/Motorcycle/Motorcycle.js
@@ -1,57 +1,65 @@
-import { Group } from 'three';
-import { MTLLoader } from 'three/examples/jsm/loaders/MTLLoader';
-import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js';
+import {
+  Group, 
+} from 'three';
+import {
+  MTLLoader
+} from 'three/examples/jsm/loaders/MTLLoader';
+import {
+  OBJLoader
+} from 'three/examples/jsm/loaders/OBJLoader.js';
 import MODEL_ONE from './1388 Motorcycle.obj';
 import MAT_ONE from './1388 Motorcycle.mtl';
 import MODEL_TWO from './Motorcycle_1388.obj';
 import MAT_TWO from './Motorcycle_1388.mtl';
-import {
-    Vector3
-  } from 'three';
 
 class Motorcycle extends Group {
-    constructor(parent, playerId) {
-        // Call parent Group() constructor
-        super();
+  constructor(parent, playerId) {
+    // Call parent Group() constructor
+    super();
 
-        // Init state
-        this.state = {
-            gui: parent.state.gui,
-        };
+    // Init state
+    this.state = {
+      gui: parent.state.gui,
+      direction: playerId === 1 ? 0 : 1,
+    };
 
-        // Load object
-        const loader = new OBJLoader();
-        const mtlLoader = new MTLLoader();
-        this.name = playerId === 1 ? 'redCycle' : 'yellowCycle';
-        const model = playerId === 1 ? MODEL_ONE : MODEL_TWO;
-        const mat = playerId === 1 ? MAT_ONE : MAT_TWO;
+    // Load object
+    const loader = new OBJLoader();
+    const mtlLoader = new MTLLoader();
+    this.name = playerId === 1 ? 'redCycle' : 'yellowCycle';
+    const model = playerId === 1 ? MODEL_ONE : MODEL_TWO;
+    const mat = playerId === 1 ? MAT_ONE : MAT_TWO;
 
-        mtlLoader.setResourcePath('src/components/objects/Motorcycle/');
-        mtlLoader.load(mat, (material) => {
-          material.preload();
-          loader.setMaterials(material).load(model, (obj) => {
-            this.add(obj);
-          });
-        });
+    mtlLoader.setResourcePath('src/components/objects/Motorcycle/');
+    mtlLoader.load(mat, (material) => {
+      material.preload();
+      loader.setMaterials(material).load(model, (obj) => {
+        this.add(obj);
+      });
+    });
 
-        // Add self to parent's update list
-        parent.addToUpdateList(this);
+    // Add self to parent's update list
+    parent.addToUpdateList(this);
+  }
+
+  updateDir(key) {
+    // 0 is up, 1 is down
+  }
+
+  update(timeStamp) {
+    switch (timeStamp.key) {
+      case "ArrowLeft":
+        this.rotateY(Math.PI);
+        break;
+      case "ArrowRight":
+        this.rotateY(-Math.PI / 2);
     }
-
-    update(timeStamp) {
-        switch (timeStamp.key) {
-            case "ArrowLeft":
-                this.rotateY(Math.PI);
-
-            case "ArrowRight":
-                this.rotateY(-Math.PI / 2);
-        }
-        // var rot = new Vector3();
-        // this.getWorldDirection(rot);
-        // const x = rot.normalize();
-        // this.position.set(this.position.clone().add(x.multiplyScalar(0.1)));
-        // const y = 0;
-    }
+    // var rot = new Vector3();
+    // this.getWorldDirection(rot);
+    // const x = rot.normalize();
+    // this.position.set(this.position.clone().add(x.multiplyScalar(0.1)));
+    // const y = 0;
+  }
 }
 
 
